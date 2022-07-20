b61aac27e6d29d3ea5da7aed8e0b6069c96836dc
crisvlasova <vlasovacristina@gmail.com>
2022-07-20 10:13:29 -0300

    addition of basic searchbar

---
+++ ecommercegrupo05/src/components/molecules/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchBar from '../atoms/seacrbar'
+
+export default function NavBar () {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    )
+}
--- ecommercegrupo05/src/components/organisms/Home.jsx
+++ ecommercegrupo05/src/components/organisms/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Slider } from '../molecules/slider';
 import style from '../../styles/carrousel.module.css'
+import NavBar from '../molecules/navbar'
 
 export default function Home () {
     return (
         <div>
+            <NavBar/>
         </div>
     )
 }
