c0fbc613054333bdbd0628de03f95ea5eb0419fe
crisvlasova <vlasovacristina@gmail.com>
2022-07-20 18:32:12 -0300

    basic navbar

--- ecommercegrupo05/src/components/atoms/sidebaroptions.jsx
+++ ecommercegrupo05/src/components/atoms/sidebaroptions.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from '../../styles/sidebaroptions.module.css'
 
 export default function SidebarOptions () {
     return (
-        <div>
+        <div className={style.options}>
             <span>Categories</span>
             <span>Favourites</span>
             <span>Cart</span>
