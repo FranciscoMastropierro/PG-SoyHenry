1b75980de68289b45bcdf857238e7b11bc166905
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-07-20 18:27:35 -0500

    making making routes and components in the application

--- ecommercegrupo05/src/App.js
+++ ecommercegrupo05/src/App.js
@@ -1,7 +1,17 @@
 import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import Home from './components/organisms/Home'
+import AllProducts from './components/organisms/allProducts';
+import Details from './components/organisms/details';
+import Login from './components/organisms/login';
+import Cart from './components/organisms/cart';
+import AboutUs from './components/organisms/aboutUs';
+import Faq from './components/organisms/FAQ';
+import NotFound from './components/organisms/notFound';
+import PaymentGateway from './components/organisms/paymentGateway';
+import News from './components/organisms/news';
 
 
 function App() {
@@ -13,7 +23,18 @@ function App() {
 
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route exact path = '/' element={<Home />} />
+        <Route exact path = '/allProducts' element={<AllProducts />} />
+        <Route exact path = '/details/:id' element={<Details />} />
+        <Route exact path = '/login' element={<Login />} />
+        <Route exact path = '/paymentGateway' element={<PaymentGateway />} />
+        <Route exact path = '/cart' element={<Cart />} />
+        <Route exact path = '/aboutUs' element={<AboutUs />} />
+        <Route exact path = '/faq' element={<Faq />} />
+        <Route exact path = '/news' element={<News />} />
+        <Route exact path = '*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ ecommercegrupo05/src/components/organisms/notFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+    
+    return (
+        <div>
+            <h1>this is NotFound components</h1>
+        </div>
+    )
+}
