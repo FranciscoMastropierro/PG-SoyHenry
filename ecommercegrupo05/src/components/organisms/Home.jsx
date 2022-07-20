4a76b48c2ea2293fd69d05ef3dbfa57315983e17
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-07-19 22:08:03 -0500

    making carrousel component

--- ecommercegrupo05/src/components/organisms/Home.jsx
+++ ecommercegrupo05/src/components/organisms/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Carrousel } from '../molecules/carrousel';
+import style from '../../styles/carrousel.module.css'
 
 export default function Home () {
     return (
-        <div>
+        <div className={style.carrousel}>
+            <Carrousel />
         </div>
     )
 }
