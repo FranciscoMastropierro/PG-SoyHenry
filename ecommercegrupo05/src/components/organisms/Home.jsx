6f66188e64375a00f6dcb6304404ca4136af6c65
crisvlasova <vlasovacristina@gmail.com>
2022-07-21 13:04:14 -0300

    adding default footer

--- ecommercegrupo05/src/components/organisms/Home.jsx
+++ ecommercegrupo05/src/components/organisms/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Slider } from '../molecules/slider';
 import style from '../../styles/carrousel.module.css'
 import NavBar from '../molecules/navbar'
+import Footer from '../molecules/footer'
 
 export default function Home () {
     return (
         <div>
             <NavBar/>
+            <Footer/>
         </div>
     )
 }
