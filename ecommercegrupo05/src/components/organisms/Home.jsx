4970ce4dbce02dd01d1a63413ddd31ff1faf51d8
OjedaMartin <martinignacioojeda@gmail.com>
2022-07-21 14:16:30 -0300

    Brandbar Front

--- ecommercegrupo05/src/components/organisms/Home.jsx
+++ ecommercegrupo05/src/components/organisms/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Slider } from '../molecules/slider';
 import style from '../../styles/carrousel.module.css'
 import NavBar from '../molecules/navbar'
+import BrandBar from '../molecules/brandbar'
+
 
 export default function Home () {
     return (
         <div>
             <NavBar/>
+            <BrandBar/>
         </div>
     )
 }
