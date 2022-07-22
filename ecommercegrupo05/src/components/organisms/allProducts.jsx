368a8e9634162d4543e27c2db35501b68542ca56
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-07-22 18:20:35 -0500

    making pagination at allProducts component

--- ecommercegrupo05/src/components/organisms/allProducts.jsx
+++ ecommercegrupo05/src/components/organisms/allProducts.jsx
@@ -1,37 +1,30 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux';
-import { getProducts } from "../../redux/actions";
-import Card from "../atoms/Card";
+import { getProducts, paginacion } from "../../redux/actions";
 import style from '../../styles/allProducts.module.css'
+import Pagination from "../atoms/paginacion";
+import CardProducts from "../atoms/cardProducts";
 
 export default function AllProducts() {
 
     const dispatch = useDispatch()
 
+    const pages = useSelector((state) => state.pages)
     const products = useSelector((state) => state.data)
 
     useEffect(() => {
         dispatch(getProducts())
     }, [dispatch])
+
+    useEffect(() => {
+        dispatch(paginacion(pages))
+    }, [dispatch, products, pages])
     
     return (
-        <div className={style.container}>
-            {
-                products && products.map(({id, image, name, price}) => (
-                    <div className={style.card_container} key={id}>
-                        <Link to={`/details/${id}`}>
-                            <div className={style.card}>
-                                <Card 
-                                image={image} 
-                                name={name}
-                                price={price}
-                            />
-                            </div>
-                        </Link>
-                    </div>
-                ))
-            }
-        </div>
+        <>
+            <Pagination />
+            <CardProducts />
+        </>
     )
 }
