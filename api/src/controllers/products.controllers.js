b32584a5817d3f36c8eb2d9c7a6e44069c5b3096
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-07-22 12:24:00 -0500

    relation frontend with backend

--- api/src/controllers/products.controllers.js
+++ api/src/controllers/products.controllers.js
@@ -10,19 +10,7 @@ module.exports = {
   getProducts: async (req, res) => {
     const { name } = req.query;
 
-<<<<<<< HEAD
-    const productsBd2 = await Products.findAll({
-      include: { model: Categories },
-    });
-
-    // se creo variable improvisada para poder recibir todos los productos y no necesite un name de manera obligatoria
-    // se requeria el name a fuerza para poder enviar todos los productos, con esta variable ya no sucede eso
-    // esto totalmente provisional
-
-    if (name) {
-=======
     if (!name) {
->>>>>>> develop
       const productsBd = await Products.findAll({
         include: { model: Categories },
       });
@@ -41,13 +29,7 @@ module.exports = {
     
       if (productsBd.length > 0) return res.send(productsBd);
       else return res.status(404).send('Product not found');
-<<<<<<< HEAD
-
-
-    } else return res.status(200).send(productsBd2);
-=======
     }
->>>>>>> develop
   },
 
   postProduct: async (req, res) => {
