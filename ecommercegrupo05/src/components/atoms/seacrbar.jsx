8269a6f57d1e63c9a4818f591b94ee2a3d9c5d6c
crisvlasova <vlasovacristina@gmail.com>
2022-07-22 19:23:45 -0300

    css changes

--- ecommercegrupo05/src/components/atoms/seacrbar.jsx
+++ ecommercegrupo05/src/components/atoms/seacrbar.jsx
@@ -5,22 +5,30 @@ import { getProductByName } from '../../redux/actions';
 import style from '../../styles/searchbar.module.css'
 import {connect} from 'react-redux'
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function SearchBar ({searchedProducts, getProductByName}) {
 
     const [products, setProducts] = useState('')
     const [value, setValue] = useState('')
 
+    let navigate = useNavigate()
+
     useEffect(() => {
         getProductByName(value)
         setProducts(searchedProducts)
     }, [value]) //eslint-disable-line react-hooks/exhaustive-deps
 
+    function handleClick (e) {
+        e.preventDefault();
+        navigate('/allProducts/asus')
+    }
+
     return (
         <div className={style.searchbarGeneralContainer}>
             <div className={style.searchbarContainer}>
                 <input key='searchbar' placeholder='search...' value={value} onChange={e => setValue(e.target.value)}/>
-                <button type='submit'><img src={search} alt='search-button'/></button>
+                <button type='submit' onClick={(e) => handleClick(e)}><img src={search} alt='search-button'/></button>
             </div>
             <div className={style.productsBox}>
                 {value && searchedProducts.map(p => { return (
