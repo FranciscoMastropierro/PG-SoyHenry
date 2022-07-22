611e2696aff7234a9eb56e9f508353742cf2fc35
César Augusto Villanueva O <lordchief21@gmail.com>
2022-07-21 22:21:54 -0500

    Adding InfoJSON Categories & Products

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 const server = require('./src/app.js');
-const preload = require('./src/controller/productsPreload.js');
+const {preload, preloadCate} = require('./src/controller/productsPreload.js');
 const { conn } = require('./src/db.js');
+
+
+
+
 const port= process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    preload()
+    preload();
+    preloadCate();
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
--- api/src/controller/productsPreload.js
+++ api/src/controller/productsPreload.js
@@ -1,5 +1,6 @@
 const productList = require('../asset/productList');
-const {Products} = require('../db')
+const {Products, Categories} = require('../db');
+
 
 const preload = () => {
     const upToDb = productList.map(async el => {
@@ -14,8 +15,41 @@ const preload = () => {
                 description: el.description.trim(),
             },
            
-        })
+        });
     })
 }
 
-module.exports = preload
+
+const preloadCate = () => {
+    try {
+    
+        let cateArr = [];
+        let cateMap = productList.map((el) => {
+            let cate = el.categories;
+            
+            cateArr.push(cate)
+        });
+
+        let cateFlat = cateArr.flat();
+
+        const cateSet = new Set(cateFlat);
+        const cateResult = Array.from(cateSet)
+
+        const cateUpToDb = cateResult.map(async el => {
+            await Categories.findOrCreate({
+                where:{name: el}
+            })
+        })
+
+        console.log(cateResult)
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+module.exports = {preload, preloadCate}
