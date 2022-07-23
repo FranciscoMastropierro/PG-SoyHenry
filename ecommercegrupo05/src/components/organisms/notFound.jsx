3203757de0981ff8570d0963d5ae69b77d50f63b
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-07-23 14:55:55 -0500

    adding style in NotFound componets

--- ecommercegrupo05/src/components/organisms/notFound.jsx
+++ ecommercegrupo05/src/components/organisms/notFound.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import style from '../../styles/notFound.module.css'
 
-export default function NotFound() {
-    
+export default function NotFound () {
+
     return (
-        <div>
-            <h1>this is NotFound components</h1>
+        <div className={style.container}>
+            <h2 className={style.h2}>
+                <span className={style.span}>ERROR</span>
+                <span className={style.span}>404</span>
+            </h2>
         </div>
     )
 }
