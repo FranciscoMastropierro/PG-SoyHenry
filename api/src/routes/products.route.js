0da6957048036ae271698a1d63d8ea0baabcb29f
FranciscoMastropierro <mastropierro,francisco@gmail.com>
2022-07-23 11:39:46 -0300

    feat(api / add products orderByName)

--- api/src/routes/products.route.js
+++ api/src/routes/products.route.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const {getProducts, postProduct,getPrice, filterByCategories} = require('../controllers/products.controllers')
+const {getProducts, postProduct,getPrice, filterByCategories, getOrderByName} = require('../controllers/products.controllers')
 
 
 router.get('/', getProducts)
@@ -8,6 +8,8 @@ router.get('/', getProducts)
 router.get('/price', getPrice)
 
 router.get('/categories', filterByCategories)
+
+router.get('/orderByName', getOrderByName)
 
 router.post('/', postProduct)
 
