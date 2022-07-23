886c34333f166795176f3e7eb8d276a86f03cd4f
crisvlasova <vlasovacristina@gmail.com>
2022-07-22 21:29:36 -0300

    background css

--- ecommercegrupo05/src/components/atoms/Card.jsx
+++ ecommercegrupo05/src/components/atoms/Card.jsx
@@ -5,8 +5,10 @@ function Card({image, name, price}) {
   return (
     <div className={style.container}>
         <img className={style.img} src={image} alt="Flag" width='130' height='100' />
-        <h4 className={style.name}>{name}</h4>
-        <h5 className={style.price}>$ {price}</h5>
+        <div className={style.textContainer}>
+          <h4 className={style.name}>{name}</h4>
+          <h5 className={style.price}>$ {price}</h5>
+        </div>
     </div>
   )
 }
