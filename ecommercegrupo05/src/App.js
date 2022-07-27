41f265592ae171c7394bd1e5c256df29ef30f01d
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-07-27 16:36:01 -0500

    making cart

--- ecommercegrupo05/src/App.js
+++ ecommercegrupo05/src/App.js
@@ -17,6 +17,7 @@ import NavBar from './components/molecules/navbar';
 import Categories from './components/organisms/categories';
 import CreateForm from './components/organisms/createForm';
 import { Whatsapp } from './components/atoms/whatsapp';
+import { CartContext } from './context/CartItem';
 
 
 
@@ -29,22 +30,22 @@ function App() {
 
   return (
     <div>
-        <NavBar/>
+      <NavBar />
         <Routes>
-        <Route exact path = '/' element={<Home />} />
-        <Route exact path = '/allProducts' element={<AllProducts />} />
-        <Route exact path = '/details/:id' element={<Details />} />
-        <Route exact path = '/login' element={<Login />} />
-        <Route exact path = '/favorites' element={<Favorites />} />
-        <Route exact path = '/paymentGateway' element={<PaymentGateway />} />
-        <Route exact path = '/cart' element={<Cart />} />
-        <Route exact path = '/aboutUs' element={<AboutUs />} />
-        <Route exact path = '/faq' element={<Faq />} />
-        <Route exact path = '/news' element={<News />} />
-        <Route exact path ='/categories' element={<Categories/>}/>
-        <Route exact path = '/create' element={<CreateForm />} />
-        <Route exact path = '*' element={<NotFound />} />
-      </Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/allProducts' element={<AllProducts />} />
+          <Route exact path='/details/:id' element={<Details />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/favorites' element={<Favorites />} />
+          <Route exact path='/paymentGateway' element={<PaymentGateway />} />
+          <Route exact path='/cart' element={<Cart />} />
+          <Route exact path='/aboutUs' element={<AboutUs />} />
+          <Route exact path='/faq' element={<Faq />} />
+          <Route exact path='/news' element={<News />} />
+          <Route exact path='/categories' element={<Categories />} />
+          <Route exact path='/create' element={<CreateForm />} />
+          <Route exact path='*' element={<NotFound />} />
+        </Routes>
       <Whatsapp />
     </div>
   );
