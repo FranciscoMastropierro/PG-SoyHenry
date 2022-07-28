7e9b34f8ccbdde7fcb1f5a0b605d6339bd0cc6e3
César Augusto Villanueva O <lordchief21@gmail.com>
2022-07-28 10:40:44 -0500

    "Structuring and Adding new User to Db"

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -23,22 +23,17 @@ module.exports = (sequelize) => {
     },
     adress:{
       type :DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     username: {
       type :DataTypes.STRING,
       allowNull: false,
       unique: true,
     },
-    password:{
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     isAdmin: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     }
-
   },
   {
     timestamps: false ,
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,6 +5,7 @@ const { requiresAuth } = require('express-openid-connect');
 const categories = require('./categories.route.js')
 const product = require('./product.route.js')
 const products = require('./products.route.js')
+const auth0 = require('./auth0.route.js')
 
 const router = Router();
 
@@ -17,28 +18,7 @@ router.use('/product', product);
 
 router.use('/products', products)
 
-router.get('/', (req, res) => {
-    console.log('Flag App Auth0', req.oidc.isAuthenticated());
-    console.log('Flag app Auth0 USR', req.oidc.user)
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
-
-
-router.get("/profile", requiresAuth(),(req, res) => {
-    
-    let arrUserInfo =  []
-    
-    const isAuthenticated=  req.oidc.isAuthenticated();
-    const user =  req.oidc.user;
-
-    user.isAuthenticated = isAuthenticated
-
-    arrUserInfo.push(user)
-
-    console.log(arrUserInfo)
-    res.send(arrUserInfo)
-
-}); 
+router.use('/', auth0)
 
 
 module.exports = router;
