70df2ece68aef77fde2b4722b9a5be60e2551d0d
crisvlasova <vlasovacristina@gmail.com>
2022-07-28 13:59:30 -0300

    final css changes

--- ecommercegrupo05/src/components/atoms/Card.jsx
+++ ecommercegrupo05/src/components/atoms/Card.jsx
@@ -27,13 +27,14 @@ function Card({image, name, price, id}) {
       <div className={style.imgContainer}>
         <img className={style.img} src={image} alt="Flag" width='130' height='100' />
         <button onClick={(e) => handleClick(e)}>
-          <img src={!fav? favourites : click} alt='favourite' className={style.favourite}/>
+          {/* <img src={!fav? favourites : click} alt='favourite' className={style.favourite}/> */}
         </button>
       </div>
       <div className={style.textContainer}>
         <h4 className={style.name}>{name}</h4>
-        <h5 className={style.price}>$ {price}</h5>
+        <h5 className={style.price}>${price}</h5>
         <button onClick={() => navigate(`/details/${id}`)}>Ver producto</button>
+        <button>Agregar a favoritos</button>
       </div>
     </div>
   )
