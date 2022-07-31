cb105e1160beaa95a992fb900b2c9466358c7117
César Augusto Villanueva O <lordchief21@gmail.com>
2022-07-30 22:13:04 -0500

    "Fix woute Auth0"

--- api/src/controllers/auth0.controllers.js
+++ api/src/controllers/auth0.controllers.js
@@ -5,48 +5,59 @@ const url = require('url')
 
 
 module.exports = {
-    authenthincateUser: async (req, res, next) => {
-        // console.log('Flag App Auth0', req.oidc.isAuthenticated());
-        // console.log('Flag app Auth0 USR', req.oidc.user);
+    // authenthincateUser: async (req, res, next) => {
+    //     // console.log('Flag App Auth0', req.oidc.isAuthenticated());
+    //     // console.log('Flag app Auth0 USR', req.oidc.user);
 
-        const user = req.oidc.user
+    //     const user = req.oidc.user
 
 
-        //    const admin = user.email === userAdminJSON.email ? true: false ---> Para generar administradores NOTA: Necesitamos un JSON con los email permitidos 
-        if (req.oidc.isAuthenticated()) {
-            Users.findOrCreate({
-                where: {
-                    firstname: user.given_name,
-                    lastname: user.family_name,
-                    username: user.nickname,
-                    email: user.email,
-                    profileImage: user.picture,
-                },
-            }).then(user => {
-                const aux = user[0]
-                res.send(aux)
-            })
-        } else {
-            res.send(req.oidc.isAuthenticated());
-        }
-    },
+    //     //    const admin = user.email === userAdminJSON.email ? true: false ---> Para generar administradores NOTA: Necesitamos un JSON con los email permitidos 
+    //     if (req.oidc.isAuthenticated()) {
+    //         Users.findOrCreate({
+    //             where: {
+    //                 firstname: user.given_name,
+    //                 lastname: user.family_name,
+    //                 username: user.nickname,
+    //                 email: user.email,
+    //                 profileImage: user.picture,
+    //             },
+    //         }).then(user => {
+    //             const aux = user[0]
+    //             res.send(aux)
+    //         })
+    //     } else {
+    //         res.send(req.oidc.isAuthenticated());
+    //     }
+    // },
 
 
 
     infoProfile: (req, res) => {
 
-        let arrUserInfo = []
+        res.send({success: true})
+        
+        
+        
+        
+        
+        
+        // let arrUserInfo = []
 
-        const isAuthenticated = req.oidc.isAuthenticated();
-        const user = req.oidc.user;
+        // const isAuthenticated = req.oidc.isAuthenticated();
+        // const user = req.oidc.user;
 
-        user.isAuthenticated = isAuthenticated
+        // user.isAuthenticated = isAuthenticated
 
-        arrUserInfo.push(user)
+        // arrUserInfo.push(user)
 
-        console.log(arrUserInfo)
-        res.send(arrUserInfo)
-    }
+        // console.log(arrUserInfo)
+        // res.send(arrUserInfo)
+    },
+
+
+
+
 
 
 }
