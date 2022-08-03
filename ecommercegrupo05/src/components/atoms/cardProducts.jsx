6bfe6b94fecfd029a0b2761eeefd2522caeedf2c
OjedaMartin <martinignacioojeda@gmail.com>
2022-08-03 12:33:21 -0300

    hot fix

--- ecommercegrupo05/src/components/atoms/cardProducts.jsx
+++ ecommercegrupo05/src/components/atoms/cardProducts.jsx
@@ -20,7 +20,7 @@ export default function CardProducts() {
 
     const productsToRender = useSelector((state) => state.productsToRender)
     
-    let {loginWithRedirect, isAuthenticated } = useAuth0()
+    //let {loginWithRedirect, isAuthenticated } = useAuth0()
     // if (!productsToRender.length) return <div className={style.loader}></div>
     if (!productsToRender.length) return <h1>no hemos encontrado los productos que buscaste</h1>
 
