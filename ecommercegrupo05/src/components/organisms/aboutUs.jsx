c6182f2788f1958ff456a614d90996c41275fe27
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-08-05 22:01:01 -0500

    adding aboutUs component

--- ecommercegrupo05/src/components/organisms/aboutUs.jsx
+++ ecommercegrupo05/src/components/organisms/aboutUs.jsx
@@ -3,22 +3,121 @@ import unknow from '../../assets/unknow.jpg';
 import style from '../../styles/aboutUs.module.css'
 
 export default function AboutUs() {
-    
+
     return (
-        <div className={style.container}>
-            <div className={style.card}>
-                <div className={style.imgBx}>
-                    <img className={style.img} src={unknow}/>
+        <>
+            <div>
+                <h2>
+                    FrontEnd
+                </h2>
+            </div>
+            <div className={style.container}>
+
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className={style.content}>
-                    <h3 className={style.h3}>Rogelio Ramirez<br/><span className={style.span}>FrontEnd Developer</span></h3>
-                    <ul className={style.sci}>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                    </ul>
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.card}>
+                    <div className={style.imgBx}>
+                        <img className={style.img} src={unknow} />
+                    </div>
+                    <div className={style.content}>
+                        <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FrontEnd Developer</span></h3>
+                        <ul className={style.sci}>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
