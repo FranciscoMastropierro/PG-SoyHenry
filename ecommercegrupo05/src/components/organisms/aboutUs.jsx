9d579e0f7e3db3e293cca3e8b910aa163690762f
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2022-08-09 17:30:30 -0500

    no es yonez es perez yonez

--- ecommercegrupo05/src/components/organisms/aboutUs.jsx
+++ ecommercegrupo05/src/components/organisms/aboutUs.jsx
@@ -66,9 +66,9 @@ export default function AboutUs() {
                     <div className={style.content}>
                         <h3 className={style.h3}>Rogelio Ramirez<br /><span className={style.span}>FullStack Developer</span></h3>
                         <ul className={style.sci}>
-                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
-                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
-                            <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-instagram"></i></a></li>
+                            <li className={style.li}><a href="https://github.com/Bortosh" className={style.a} target="_blank"><i class="fa-brands fa-github"></i></a></li>
+                            <li className={style.li}><a href="https://www.linkedin.com/in/rogelio-ramirez-carmona/" className={style.a} target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li className={style.li}><a href="#" className={style.a} target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
                         </ul>
                     </div>
                 </div>
@@ -127,7 +127,7 @@ export default function AboutUs() {
                         <img className={style.img} src={rodrigo} />
                     </div>
                     <div className={style.content}>
-                        <h3 className={style.h3}>Rodrigo Yones<br /><span className={style.span}>FullStack Developer</span></h3>
+                        <h3 className={style.h3}>Rodrigo Perez Yones<br /><span className={style.span}>FullStack Developer</span></h3>
                         <ul className={style.sci}>
                             <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-github"></i></a></li>
                             <li className={style.li}><a href="#" className={style.a}><i class="fa-brands fa-linkedin"></i></a></li>
